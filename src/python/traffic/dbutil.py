a001916dfc16463f5ca422996dd97e4e67a594ed
Trevor Blanarik <tblanarik@gmail.com>
2018-05-20 12:16:03 -0700

    making timezones a little more friendly

--- src/python/traffic/dbutil.py
+++ src/python/traffic/dbutil.py
@@ -1,6 +1,10 @@
-import pyodbc 
 import configparser
+import datetime
+
+import dateutil
 import pandas
+
+import pyodbc
 
 config = configparser.ConfigParser()
 config.read('../../../_private/db.config')
@@ -14,9 +18,31 @@ connection_string = ('Driver={{ODBC Driver 13 for SQL Server}};'
                      'TrustServerCertificate=no;'
                      'Connection Timeout=30;').format(**config['CONNECTION'])
 
-def get_rows(tid):
-    tid = int(tid)
-    conn = pyodbc.connect(connection_string)
-    query = "SELECT TimeUpdated,CurrentTime,Description FROM TravelTimes WHERE TRAVELTIMEID = {0}".format(tid)
-    df =  pandas.read_sql_query(query, conn)
+def _format_date_as_string(dt, local_tz):
+    if type(dt) == str:
+        local_zone = dateutil.tz.gettz(local_tz)
+        utc_zone = dateutil.tz.gettz('UTC')
+        return dateutil.parser.parse(dt).replace(tzinfo=local_zone).astimezone(utc_zone).strftime('%Y-%m-%dT%H:%M:%S')
+    if type(dt) == datetime.datetime:
+        return dt.strftime('%Y-%m-%dT%H:%M:%S')
+
+def get_travel_times(tid, startdate='1970-01-01', enddate='3000-01-01', local_tz = 'US/Pacific'):
+    """Get the travel times for a route between given times
+    Returns a Pandas DataFrame. The TimeUpdated field will be converted to the timezone specified
+    in local_tz.
+    The startdate and enddate fields are assumed to be in the same timezone as local_tz
+
+    tid - TravelTimeID, the ID in the WSDOT API for the desired route
+    startdate - a string or datetime object, assumed to be in the same tz as local_tz, specifying the lower bound (inclusive) for the query
+    enddate - a string or datetime object, assumed to be in the same tz as local_tz, specifying the upper bound (inclusive) for the query
+    """
+    from_zone = dateutil.tz.gettz('UTC')
+    to_zone = dateutil.tz.gettz(local_tz)
+    startdate = _format_date_as_string(startdate, local_tz)
+    enddate = _format_date_as_string(enddate, local_tz)
+
+    query = "SELECT TimeUpdated,CurrentTime,Description FROM TravelTimes WHERE TRAVELTIMEID = {0} AND TIMEUPDATED BETWEEN '{1}' AND '{2}'".format(tid, startdate, enddate)
+    with pyodbc.connect(connection_string) as conn:
+        df =  pandas.read_sql_query(query, conn)
+    df['TimeUpdated'] = df['TimeUpdated'].dt.tz_localize(from_zone).dt.tz_convert(to_zone)
     return df.sort_values(by=['TimeUpdated'])
--- src/python/traffic/plotting.py
+++ src/python/traffic/plotting.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.dates import DateFormatter
+import dateutil
 
-def canvas_from_data(rows):
+def canvas_from_data(rows, local_tz='US/Pacific'):
+    local_zone = dateutil.tz.gettz(local_tz)
     name = rows['Description'][0].strip()
     fig=Figure()
     ax=fig.add_subplot(111)
     x = rows['TimeUpdated']
     y = rows['CurrentTime']
     ax.plot_date(x, y, '-')
-    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M'))
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M', tz=local_zone))
     ax.set_title(name)
     fig.autofmt_xdate()
     canvas=FigureCanvas(fig)
